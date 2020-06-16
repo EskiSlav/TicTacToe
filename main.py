13e10ec9c59d299a87ca899eb44a6759783c7752
EskiSlav <uberbug544@gmail.com>
2020-06-16 18:07:12 +0300

    Bug fixes and adding comment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ ROWS, COLS = 3, 3
 wnd = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
 
+#images used (circle, cross)
 cross = pygame.image.load("Bullets/X_2D_Orange.png")
 circle = pygame.image.load("Bullets/Disc_2D_Red.png")
 
@@ -30,12 +31,13 @@ circle = resize_image(circle, int(WIDTH / ROWS))
 
 class Label:
     def __init__(self, text, x, y, font_size=30):
-        self.font = pygame.font.Font("fonts/Sen-Bold.ttf", font_size) 
-        self.text = text
-        self.text_surf = self.font.render(text, True, BLACK)
-        self.text_rect = self.text_surf.get_rect()
-        self.text_rect.center = (x, y)
+        self.font = pygame.font.Font("fonts/Sen-Bold.ttf", font_size) #load font
+        self.text = text # save text
+        self.text_surf = self.font.render(text, True, BLACK) #render text
+        self.text_rect = self.text_surf.get_rect()           # get rect from text to change its position
+        self.text_rect.center = (x, y)                       # changing text position to (x, y)
 
+        # creating GRAY background and change its position to (x, y)
         self.back_rect = pygame.Rect(0, 0, self.text_rect.width + 50, self.text_rect.height + 20)
         self.back_rect.center = (x, y)
     
@@ -57,6 +59,7 @@ class Button:
         wnd.blit(self.text_surf, self.text_rect)
 
     def collidepoint(self, x, y):
+        """ Checks if button is pressed """
         if self.back_rect.collidepoint(x, y):
             return True
         return False
@@ -71,7 +74,7 @@ class ExitButton(Button):
 
 class ResetButton(Button):
     def __init__(self, text):
-        x = WIDTH/4
+        x = WIDTH/4 
         y = HEIGHT/2 - 20
         button_width = WIDTH/4
         button_height =  40
@@ -85,14 +88,13 @@ class Playground():
         self.width  = playground_width
         self.height = playground_height
         
-
         # 1 - time to draw cross (tick)
         # 2 - time to draw circle 
         # cross moves first 
         self.turn   = 1 # cross (tick)
 
         self.rects  = []
-        self.states = []
+        self.states = [] # state that shows is there placed rect, circle or nothing
         self.create_playground()
 
     def create_playground(self, rows=3, cols=3):
@@ -104,6 +106,8 @@ class Playground():
             self.states.append([ 0 for _ in range(rows) ])
 
     def clear_playground(self, n=3, m=3):
+        """ Clears Playground after the end of the game """
+        self.turn   = 1
         self.rects  = []
         self.states = []
         self.create_playground()
@@ -123,6 +127,10 @@ class Playground():
 
 
     def draw(self, rows=3, cols=3):
+        """ Draws all lines, circles and crosses """
+        
+        # Checking all rect and drawing circle or cross
+        # based on what value is stored in self.states[i][j]
         for rects_row, states_row in zip(self.rects, self.states):
             for rect, state in zip(rects_row, states_row):
                 if state == 1:
@@ -130,9 +138,10 @@ class Playground():
                 elif state == 2:
                     wnd.blit(circle, rect)
 
-        width  = self.width / rows 
-        height = self.height / cols
+        width  = self.width / rows  # width  of single rect
+        height = self.height / cols # height of single rect
 
+        #Drawing lines
         for row in range(rows):
             pygame.draw.line(wnd, GRAY, (0, row * width), (WIDTH, row * width))
 
@@ -141,19 +150,20 @@ class Playground():
 
     def check_win(self):
         """ Dummy way but it works"""
-        if self.check_draw():
-            print("Draw")
-            return self.check_draw()
-        elif self.check_figure(1):
+        if self.check_figure(1):
             print("Cross has won!")
             return self.check_figure(1)
         elif self.check_figure(2):
             print("Circle has won!")
             return self.check_figure(2)
+        elif self.check_draw():
+            print("Draw")
+            return self.check_draw()
         
         return False
 
     def check_draw(self):
+        """ Checking whether all rects are filled """
         for row in self.states:
             for state in row:
                 if not state:
@@ -162,6 +172,7 @@ class Playground():
             return -1
 
     def check_figure(self, figure):
+        """ Checking if there is winning combination for cross or circle """
         if (self.states[0][0] == figure and self.states[0][1] == figure and self.states[0][2] == figure) or \
            (self.states[1][0] == figure and self.states[1][1] == figure and self.states[1][2] == figure) or \
            (self.states[2][0] == figure and self.states[2][1] == figure and self.states[2][2] == figure) or \
@@ -194,6 +205,8 @@ def game_menu(display_text: str):
                         if button.collidepoint(*event.pos):
                             if button.action():
                                 return
+        
+        # Draw all the things
         ExB.draw()
         ReB.draw()
         Caption.draw()
@@ -219,6 +232,7 @@ def game_loop():
         
         wnd.fill(WHITE)
         pg.draw()
+        
         if pg.check_win() == 1:
             game_menu("X Won!")
             pg.clear_playground()
